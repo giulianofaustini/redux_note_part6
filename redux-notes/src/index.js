b59ca0c7e7c7b221006403b57686bff9b1605971
Giuliano Faustini <giuliperus@gmail.com>
2023-10-26 07:20:35 +0300

    Initial state added

--- redux-notes/src/index.js
+++ redux-notes/src/index.js
@@ -7,23 +7,7 @@ import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
