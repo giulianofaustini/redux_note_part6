6d15d7b47625085b778453fa5085f0640bfd4388
Giuliano Faustini <giuliperus@gmail.com>
2023-10-26 07:59:57 +0300

    Added filterReducer

--- redux-notes/src/App.js
+++ redux-notes/src/App.js
@@ -1,13 +1,37 @@
-import NewNote from './components/NewNote'
-import Notes from './components/Notes'
+import NewNote from "./components/NewNote";
+import Notes from "./components/Notes";
 
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value);
+  };
+
   return (
     <div>
       <NewNote />
-      <Notes  />
+      <div>
+        all{" "}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected("ALL")}
+        />
+        important{" "}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected("IMPORTANT")}
+        />
+        nonimportant{" "}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected("NONIMPORTANT")}
+        />
+      </div>
+      <Notes />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
