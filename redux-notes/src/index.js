ea8757fca284ef7fe238eb02d546fa8fff8218fa
Giuliano Faustini <giuliperus@gmail.com>
2023-10-26 15:24:18 +0300

    State management with redux toolkit

--- redux-notes/src/index.js
+++ redux-notes/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
+
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
+
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
 
-const reducer = combineReducers({
+
+const store = configureStore({
+  reducer:{
   notes: noteReducer,
   filter: filterReducer
+}
 })
-
-const store = createStore(reducer)
 
 console.log(store.getState())
 
--- redux-notes/src/reducers/noteReducer.js
+++ redux-notes/src/reducers/noteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit'
+
 const initialState = [
   {
     content: 'reducer defines how redux store works',
@@ -11,45 +13,110 @@ const initialState = [
   },
 ]
 
-const noteReducer = (state = initialState, action) => {
-  console.log('ACTION: ', action)
-  switch(action.type) {
-    case 'NEW_NOTE':
-      return [...state, action.payload]
-    case 'TOGGLE_IMPORTANCE':
-      const id = action.payload.id
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+
+const noteSlice = createSlice({
+  name: 'notes',
+  initialState,
+  reducers: {
+    createNote(state, action) {
+      console.log(JSON.parse(JSON.stringify(state)))    
+      const content = action.payload
+      state.push({
+        content,
+        important: false,
+        id: generateId(),
+      })
+    },
+    toggleImportanceOf(state, action) {
+      console.log(JSON.parse(JSON.stringify(state)))   
+      const id = action.payload
       const noteToChange = state.find(n => n.id === id)
       const changedNote = { 
         ...noteToChange, 
         important: !noteToChange.important 
       }
+      
       return state.map(note =>
         note.id !== id ? note : changedNote 
       )
-    default:
-      return state
+      
     }
-  } 
+  },
+})
 
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
+export const { createNote, toggleImportanceOf } = noteSlice.actions
+export default noteSlice.reducer
 
-export const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    payload: {
-      content,
-      important: false,
-      id: generateId()
-    }
-  }
-}
 
-export const toggleImportanceOf = (id) => {
-  return {
-    type: 'TOGGLE_IMPORTANCE',
-    payload: { id }
-  }
-}  
 
-export default noteReducer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const initialState = [
+//   {
+//     content: 'reducer defines how redux store works',
+//     important: true,
+//     id: 1,
+//   },
+//   {
+//     content: 'state of store can contain any data',
+//     important: false,
+//     id: 2,
+//   },
+// ]
+
+// const noteReducer = (state = initialState, action) => {
+//   console.log('ACTION: ', action)
+//   switch(action.type) {
+//     case 'NEW_NOTE':
+//       return [...state, action.payload]
+//     case 'TOGGLE_IMPORTANCE':
+//       const id = action.payload.id
+//       const noteToChange = state.find(n => n.id === id)
+//       const changedNote = { 
+//         ...noteToChange, 
+//         important: !noteToChange.important 
+//       }
+//       return state.map(note =>
+//         note.id !== id ? note : changedNote 
+//       )
+//     default:
+//       return state
+//     }
+//   } 
+
+// const generateId = () =>
+//   Number((Math.random() * 1000000).toFixed(0))
+
+// export const createNote = (content) => {
+//   return {
+//     type: 'NEW_NOTE',
+//     payload: {
+//       content,
+//       important: false,
+//       id: generateId()
+//     }
+//   }
+// }
+// export const toggleImportanceOf = (id) => {
+//   return {
+//     type: 'TOGGLE_IMPORTANCE',
+//     payload: { id }
+//   }
+// }  
+
+// export default noteReducer
